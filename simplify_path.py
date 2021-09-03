6e4628963fbb0e6e0a7ed989efdbc7e528051e66
YILINQ <victlin@outlook.com>
2021-09-04 00:04:30 +0800

    ass

--- simplify_path.py
+++ simplify_path.py
@@ -2,11 +2,10 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         levels = path.split('/')
         stack = []
-        print(levels)
         for level in levels:
             if level != '':
                 flag = True
-                while stack != [] and level == '..' or level == '.':
+                while stack != [] and (level == '..' or level == '.'):
                     current = stack.pop()
                     if level == "..":
                         flag = False
@@ -16,11 +15,9 @@ class Solution:
                         flag = False
                         break
                         # level = directory or level = .
-                if flag:
+                if flag and level != '.' and level != '..':
                     stack.append(level)
         result = ('/' + '/'.join(stack))
-        if result[-1] == '/':
+        if result[-1] == '/' and result != '/':
             result = result[:-1]
         return result
-
-
