d8a8cd33a38fdd3facc0935899e93e3b41f22a43
YILINQ <victlin@outlook.com>
2021-09-05 01:24:28 +0800

    ass

---
+++ BST(pre, in, post - order traversal).py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        current = root
+        while True:
+            if current:
+                stack.append(current)
+                current = current.left
+            elif stack != []:
+                current = stack.pop()
+                result.append(current.val)
+                current = current.right
+            else:
+                break
+        return result
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        visited = set()
+        current = root
+        while current and current not in visited:
+            if current.left and current.left not in visited:
+                current = current.left
+
+            elif current.right and current.right not in visited:
+                current = current.right
+
+            else:
+                result.append(current.val)
+                visited.add(current)
+                current = root
+        return result
